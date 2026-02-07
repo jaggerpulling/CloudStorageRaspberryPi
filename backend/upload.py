3c23127fd5f00f9893fe8945ac4d1411907b5ebf
Jagger Pulling <mcjagger500@gmail.com>
2026-02-06 23:54:25 -0600

    commented out logic

---
+++ backend/upload.py
@@ -0,0 +1,8 @@
+#check for file upload
+#currently doing so by monitoring file directory
+
+#upon file appearing 
+
+#send post request to fastAPI
+
+#do something with sql lite
---
+++ frontend/CLIinterface.py
@@ -0,0 +1,2 @@
+#temporary command line interface to test API requests without needing
+#full frontend
