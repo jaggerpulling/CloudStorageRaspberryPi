961348a3a73af1043e02be06164e00807b2b33f0
jaggerpulling <mcjagger500@gmail.com>
2026-02-08 23:21:23 -0600

    api upload progress

--- backend/upload.py
+++ backend/upload.py
@@ -1,4 +1,23 @@
-import fastapi
+from fastapi import FastAPI
+
+app = FastAPI()
+
+ 
+@app.get("/") #whenever someone makes a get request run the function
+async def root(): #called whenever it receives request to url "/" using a GET operation
+    return {"message": "Hello World"}
+
+@app.post("/upload file")
+async def file_upload() 
+
+#common operations
+"""
+POST: to create data.
+GET: to read data.
+PUT: to update data.
+DELETE: to delete data.
+
+"""
 
 #check for file upload
 #currently doing so by monitoring file directory
